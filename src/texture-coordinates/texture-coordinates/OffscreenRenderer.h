ad134a818e287b43cec8d84bc347a82aa32460d1
Christoph Halang <christoph.halang@gmail.com>
2015-03-02 20:55:42 +0100

    added offscreen render class and image view

---
+++ src/texture-coordinates/texture-coordinates/OffscreenRenderer.h
@@ -0,0 +1,13 @@
+//
+//  OffscreenRenderer.h
+//  texture-coordinates
+//
+//  Created by Christoph Halang on 02/03/15.
+//  Copyright (c) 2015 Christoph Halang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OffscreenRenderer : NSObject
+
+@end
---
+++ src/texture-coordinates/texture-coordinates/OffscreenRenderer.m
@@ -0,0 +1,13 @@
+//
+//  OffscreenRenderer.m
+//  texture-coordinates
+//
+//  Created by Christoph Halang on 02/03/15.
+//  Copyright (c) 2015 Christoph Halang. All rights reserved.
+//
+
+#import "OffscreenRenderer.h"
+
+@implementation OffscreenRenderer
+
+@end
